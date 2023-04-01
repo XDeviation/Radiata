4a31039c8be1779802d403c3e725aa0370879427
XDeviation <191602098@qq.com>
2023-04-01 11:09:46 +0800

    upd

--- src/backend/backend/main.py
+++ src/backend/backend/main.py
@@ -1,4 +1,5 @@
 import uvicorn
+import multiprocessing
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from backend.routers import auth, heartbeat, judge, sandbox, problem
@@ -24,4 +25,5 @@ app.include_router(problem.router)
 
 
 def main():
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True, workers=2)
+    multiprocessing.freeze_support()
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True, workers=10)
